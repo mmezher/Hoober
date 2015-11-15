38d61f8c37e0854432070926bc3801ddb337476b
MacLean Sochor <mas3ju@virginia.edu>
2015-11-15 02:11:02 +0000

    LOOK AT ME NOW DAD

--- db/migrate/20151104233904_create_posts.rb
+++ db/migrate/20151104233904_create_posts.rb
@@ -3,6 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.references :user, index: true, foreign_key: true
       t.string :image_id
+      t.string :place
 
       t.timestamps null: false
     end
